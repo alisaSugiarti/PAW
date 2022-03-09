baa9ae518aff7c25399a48861f4915613385bbae
alisaSugiarti <alisasugiarti@gmail>
2022-03-09 22:16:00 +0700

    add file variable.php

---
+++ variable.php
@@ -0,0 +1,12 @@
+<?php
+$a = 4;
+$b = 6;
+$c = 7;
+$penjumlahan = $a + $b;
+$pengurangan = $c - $a;
+$perkalian = $b * $c;
+
+echo"hasil penjumlahan = $penjumlahan </br>";
+echo"hasil pengurangan = $pengurangan </br>";
+echo"hasil perkalian = $perkalian </br>";
+?>
